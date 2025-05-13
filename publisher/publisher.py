05d76cb6778c073a4480b47f77addbe9c1f0bac3
Reshie <reo040125@gmail.com>
2025-05-13 22:36:13 +0900

    slackAPIの処理を作成

--- publisher/publisher.py
+++ publisher/publisher.py
@@ -1,20 +1,93 @@
 import os
+import json
+import requests
 from google.cloud import pubsub_v1
 from flask import Flask, request
+from slack_sdk import WebClient
+from pprint import pprint
 
 app = Flask(__name__)
 
-project_id = os.environ["PUBSUB_PROJECT_ID"]
-topic_id = os.environ["PUBSUB_TOPIC_ID"]
+@app.route('/message', methods=['GET'])
+def send_message():
+    client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(project_id, topic_id)
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": "Hello! Press any button😎",
+            }
+        },
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "A",
+                    },
+                    "value": "A",
+                    "action_id": "action_a"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "B",
+                    },
+                    "value": "B",
+                    "action_id": "action_b"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "C",
+                    },
+                    "value": "C",
+                    "action_id": "action_c"
+                }
+            ]
+        }
+    ]
+
+    try:
+        client.chat_postMessage(
+            channel="#general",
+            blocks=blocks
+        )
+    except Exception as e:
+        return f'Error sending message: {e}\n', 500
+    
+    return 'Message has been sent.\n', 200
+
+# @app.route('/slack/actions', methods=['POST'])
+# def handle_slack_actions():
+#     payload = json.loads(request.form.get('payload', '{}'))
+#     pprint(payload)
+#     return '', 200
 
 @app.route('/publish', methods=['POST'])
 def publish_message():
-    future = publisher.publish(topic_path, request.data)
-    future.result()
-    return 'Message published.'
+    project_id = os.environ["PUBSUB_PROJECT_ID"]
+    topic_id = os.environ["PUBSUB_TOPIC_ID"]
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
+
+    try:
+        payload = json.loads(request.form.get('payload', '{}'))
+        data = json.dumps(payload).encode('utf-8')
+        future = publisher.publish(topic_path, data)
+        future.result()
+    except Exception as e:
+        print(f'Error publishing message: {e}\n', flush=True)
+        return f'Error publishing message: {e}\n', 500
+    
+    return 'Message published.\n', 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- subscriber/subscriber.py
+++ subscriber/subscriber.py
@@ -1,5 +1,7 @@
 import os
+import json
 from google.cloud import pubsub_v1
+from slack_sdk import WebClient
 
 project_id = os.environ["PUBSUB_PROJECT_ID"] 
 subscription_id = os.environ["PUBSUB_SUBSCRIPTION_ID"]
@@ -8,7 +10,34 @@ subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 def callback(message):
-    print(f"Received {message.data.decode('utf-8')}.")
+    client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+
+    data = json.loads(message.data.decode("utf-8"))
+
+    channel_id = data["container"]["channel_id"]
+    ts = data["container"]["message_ts"]
+    selected_option = data["actions"][0]["value"]
+    blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": f"Button {selected_option} was pressed!"
+			}
+		}
+	]
+
+    try:
+        client.chat_update(
+            channel=channel_id,
+            ts=ts,
+            blocks=blocks,
+            text=f"Button {selected_option} was pressed!"
+        )
+    except Exception as e:
+        print(f"Error updating message: {e}\n")
+
+    print("Message was updated.\n")
     message.ack()
 
 streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
